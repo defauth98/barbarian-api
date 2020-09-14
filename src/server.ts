bce086f15fccde4b4032ca0fa708d66e3bc3c04c
defauth98 <neto.daniribeiro@gmail.com>
2020-09-13 21:36:15 -0300

    Configuraçao inicial do projeto

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import routes from './routes';
+
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => console.log(`Running on ${port}`));
