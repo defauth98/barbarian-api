cd301ef4bc3471ca19cca7f99cbe57e1264e8da5
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-03-19 12:46:10 -0300

    configura a variavel PORT

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import dotenv from 'dotenv'
 
 import routes from "./routes";
 
+dotenv.config()
+
 const app = express();
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(routes);
