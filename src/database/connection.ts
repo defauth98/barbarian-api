1098a2874835fdbfc3c395d161dca9dd97f686dc
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-03-19 12:49:43 -0300

    arquivo de conexao knex com variavies de ambiente

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -4,10 +4,11 @@ const db = knex({
   client: "pg",
   version: "7.2",
   connection: {
-    host: "localhost",
-    user: "postgres",
-    password: "docker",
-    database: "barbarian",
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    ssl: { rejectUnauthorized: false }
   },
 });
 
