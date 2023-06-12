aa531de537c127c5de1b72ff18e51f49830a77d8
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2023-06-11 23:19:37 -0300

    chore: remove old project files

--- src/utils/convertHourTominutes.ts
+++
@@ -1,7 +0,0 @@
-export default function covertHourToMinutes(timer: string) {
-  const [hour, minutes] = timer.split(":").map(Number);
-
-  const timeInMinutes = hour * 60 + minutes;
-
-  return timeInMinutes;
-}
